a4e693e7ca395b50d849b394d69aad1d46ac71bc
Kunal <kunal.k.kakkad@gmail.com>
2017-04-27 17:43:34 +0530

    - First commit.

---
+++ CRUDwithWebAPIandAngular.Web/Controllers/Api/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CRUDwithWebAPIandAngular.Web.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IHttpActionResult Get()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+
+        // GET api/values/5
+        public IHttpActionResult Get(int id)
+        {
+            return Ok("value" + id.ToString());
+        }
+
+        // POST api/values
+        public IHttpActionResult Post([FromBody]string value)
+        {
+            return Ok();
+        }
+
+        // PUT api/values/5
+        public IHttpActionResult Put(int id, [FromBody]string value)
+        {
+            return Ok();
+        }
+
+        // DELETE api/values/5
+        public IHttpActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
