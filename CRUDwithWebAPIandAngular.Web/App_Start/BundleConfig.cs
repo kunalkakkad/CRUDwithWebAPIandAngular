714234e45826391d4fcb07c7ef797497183a8c74
Kunal <kunal.k.kakkad@gmail.com>
2017-04-29 14:21:06 +0530

    - initial implementation of http service factory. - Bundling managed.

--- CRUDwithWebAPIandAngular.Web/App_Start/BundleConfig.cs
+++ CRUDwithWebAPIandAngular.Web/App_Start/BundleConfig.cs
@@ -9,20 +9,38 @@ namespace CRUDwithWebAPIandAngular.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/lib/jquery/jquery-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/lib/modernizr/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/lib/bootstrap/bootstrap.min.js",
+                      "~/Scripts/lib/respond/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/bootstrap.min.css",
+                      "~/Content/css/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                     "~/Scripts/lib/angular/angular.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/appStart").Include(
+                      "~/Scripts/app/appStart/appModule.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/appLogics").Include(
+                      "~/Scripts/app/logic/appDirectives.js",
+                      "~/Scripts/app/logic/serviceManager.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/controllers").Include(
+                      "~/Scripts/app/controllers/appCtrl.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/dataservices").Include(
+                      "~/Scripts/app/dataservice/appDs.js"));
+
+
         }
     }
 }
